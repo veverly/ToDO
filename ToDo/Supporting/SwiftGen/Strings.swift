dd5ae7e0aa0f400c3220a670ececad85355f943c
Veverly Mae Ricaza <veverlymaericaza@Veverlys-MacBook-Pro.local>
2021-10-08 16:10:40 +0800

    Add colors

--- ToDo/Supporting/SwiftGen/Strings.swift
+++ ToDo/Supporting/SwiftGen/Strings.swift
@@ -10,6 +10,48 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+
+  internal enum SignIn {
+    internal enum Label {
+      /// Don't have an
+      internal static let accountMessage = L10n.tr("Localizable", "sign_in.label.accountMessage")
+      /// Log in
+      internal static let login = L10n.tr("Localizable", "sign_in.label.login")
+      /// Remember me
+      internal static let rememberMe = L10n.tr("Localizable", "sign_in.label.rememberMe")
+    }
+  }
+
+  internal enum SignUp {
+    internal enum Button {
+      /// Account?
+      internal static let account = L10n.tr("Localizable", "sign_up.button.account")
+    }
+    internal enum Label {
+      /// Already have an
+      internal static let accountMessage = L10n.tr("Localizable", "sign_up.label.accountMessage")
+      /// Address
+      internal static let address = L10n.tr("Localizable", "sign_up.label.address")
+      /// Age
+      internal static let age = L10n.tr("Localizable", "sign_up.label.age")
+      /// Confirm Password
+      internal static let confirmPassword = L10n.tr("Localizable", "sign_up.label.confirmPassword")
+      /// Email
+      internal static let email = L10n.tr("Localizable", "sign_up.label.email")
+      /// First Name
+      internal static let firstName = L10n.tr("Localizable", "sign_up.label.firstName")
+      /// Last Name
+      internal static let lastName = L10n.tr("Localizable", "sign_up.label.lastName")
+      /// Middle Name
+      internal static let middleName = L10n.tr("Localizable", "sign_up.label.middleName")
+      /// Mobile
+      internal static let mobile = L10n.tr("Localizable", "sign_up.label.mobile")
+      /// Password
+      internal static let password = L10n.tr("Localizable", "sign_up.label.password")
+      /// Sign Up
+      internal static let signup = L10n.tr("Localizable", "sign_up.label.signup")
+    }
+  }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
--- ToDo/Views/SignIn/SignInViewController.swift
+++ ToDo/Views/SignIn/SignInViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignInViewController: UIViewController {
 
     private var slantedView: SlantedView = SlantedView()
+    let gradient = CAGradientLayer()
 
     override func loadView() {
         super.loadView()
@@ -22,10 +24,19 @@ class SignInViewController: UIViewController {
 
     private func setupScene() {
         setupSlantedView()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
     }
 
     private func setupSlantedView() {
         view.addSubview(slantedView)
+        slantedView.layer.cornerRadius = 30
+        slantedView.backgroundColor = .green
+        slantedView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.left.right.equalToSuperview().inset(10)
+            make.bottom.equalToSuperview().inset(50)
+        }
     }
 
 }
